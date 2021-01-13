34204dd85172880a08df333baea5f67fc1046432
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-01-13 14:30:11 +0100

    remove unecessary public path

--- webpack.serve.config.babel.js
+++ webpack.serve.config.babel.js
@@ -4,8 +4,7 @@ import { join } from 'path';
 export default {
   ...baseConfig,
   devServer: {
-    publicPath: 'dist/cdn', // here's the change
-    contentBase: join(__dirname, 'dist/cdn'),
+    contentBase: join(__dirname, '/dist/cdn'),
     host: '0.0.0.0',
     compress: true,
     hot: true,
