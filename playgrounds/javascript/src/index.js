c4f97d1c94ded02f1e8c68b8671163e361f3ba21
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-06-04 11:41:24 +0200

    Update playground

---
+++ playgrounds/javascript/src/index.js
@@ -0,0 +1,18 @@
+import docsSearchBar from '../../../index';
+import '../../../src/styles/main.scss';
+
+docsSearchBar({
+  // Test with MeiliSearch Documentation
+  hostUrl: 'https://docs-search-bar.meilisearch.com',
+  apiKey: 'd79226ae89f29d4dadba8d0c30c240e435f584fb83a7ae573b13eb62edec35cd',
+  indexUid: 'docs',
+  inputSelector: '#q',
+  handleSelected(input, event, suggestion, datasetNumber, context) {
+    console.info(input);
+    console.info(event);
+    console.info(suggestion);
+    console.info(datasetNumber);
+    console.info(context);
+  },
+  debug: true, // Set debug to true if you want to inspect the dropdown
+});
