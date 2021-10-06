4ca0caf9bf760828bb5d6fc43b843601c2b28af5
Daniel Sobczak <20947896+TheLaw1337@users.noreply.github.com>
2021-10-06 18:28:52 +0200

    Tests for visibility of searchbar and autocomplete

--- cypress/integration/first_tests.specs.js
+++ cypress/integration/first_tests.specs.js
@@ -8,4 +8,18 @@ describe(`My first test`, () => {
   it('Should visit the dashboard', () => {
     cy.url().should('match', new RegExp(HOST)) // The current host URL
   })
+
+  it('Should check if the searchbar is visible', () => {
+    cy.get('.searchbox')
+      .should('be.visible')
+  })
+
+  it('Should check if autocomplete is visible during typing', () => {
+    cy.get('#docs-searchbar-suggestion')
+      .type('Quick')
+    cy.get('#meilisearch-autocomplete-listbox-0')
+      .should('be.visible')
+    cy.get('div.dsb-suggestion:nth-child(1)') // the first entry should be "Quick start"
+      .contains('Quick start')
+  })
 })
