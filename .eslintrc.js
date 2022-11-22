80c2ea9cb052f74e822fee01042b0170b4dd7ac8
hirohe <hesreg@hotmail.com>
2022-11-22 11:42:22 +0800

    chore: typescript related eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 /* Using eslint-config-algolia */
 /* eslint-disable import/no-commonjs */
 module.exports = {
+  parser: '@typescript-eslint/parser',
   extends: [
     'algolia',
     'algolia/jest',
     'plugin:prettier/recommended',
     'plugin:cypress/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
-  plugins: ['prettier', 'cypress'],
+  plugins: ['prettier', 'cypress', '@typescript-eslint'],
   rules: {
     'no-console': 0,
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
     'prettier/prettier': [
       'error',
