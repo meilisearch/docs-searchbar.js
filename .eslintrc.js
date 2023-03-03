dfd8feba31c0940c94682d289fb7800adc812d2c
hirohe <hesreg@hotmail.com>
2023-03-03 19:13:44 +0800

    refactor: add overrides in eslint for typescript rules only

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,15 @@
 /* Using eslint-config-algolia */
 /* eslint-disable import/no-commonjs */
 module.exports = {
-  parser: '@typescript-eslint/parser',
   extends: [
     'algolia',
     'algolia/jest',
     'plugin:prettier/recommended',
     'plugin:cypress/recommended',
-    'plugin:@typescript-eslint/recommended',
   ],
-  plugins: ['prettier', 'cypress', '@typescript-eslint'],
+  plugins: ['prettier', 'cypress'],
   rules: {
     'no-console': 0,
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
     'prettier/prettier': [
       'error',
@@ -21,4 +18,34 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      extends: [
+        'algolia',
+        'algolia/jest',
+        'plugin:prettier/recommended',
+        'plugin:cypress/recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      plugins: ['prettier', 'cypress', '@typescript-eslint'],
+      rules: {
+        'no-console': 0,
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_' },
+        ],
+        'eslint-comments/disable-enable-pair': [
+          'error',
+          { allowWholeFile: true },
+        ],
+        'prettier/prettier': [
+          'error',
+          {
+            trailingComma: 'all',
+          },
+        ],
+      },
+    },
+  ],
 }
